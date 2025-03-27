41e40a9b2879ecef4b70cf5bc7d046c91ea282a3
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-03-27 23:15:11 +0100

    Manual and live react crypto-tracker and 1st done with manual component when useState used as MVP now further will use useEffect to fetch realtime data

---
+++ src/App.jsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+
+function CryptoContainer() {
+  const [ crypto, setCrypto ] = useState(0);
+
+  //add stock by 1
+  const addCrypto = () => {
+    setCrypto(crypto + 1);
+  }
+
+  //deduct crrent stock by 1
+  const deductCrypto = () => {
+
+    if (crypto>0){
+      setCrypto(crypto - 1);
+    }
+  }
+    // jump - add stock by 5 without clicking manually
+    const jumpadd5 = () => {
+      setCrypto(crypto + 5);
+    }
+
+    // jump - deduct stock by 5 (avoiding multiple manual clicks)
+    const  jumpdeduct5 = () => {
+      if (crypto>5) {
+        setCrypto(crypto - 5);
+      } else if (crypto>0){
+        setCrypto(0);
+      }
+    }
+
+    useEffect (() => {
+      console.log( `crypto stock changed : ${crypto}`)
+    });
+
+  
+
+  return(
+    <div>
+      <h2>Current crypto : {crypto} </h2>
+      <button style={{color: 'green', border: '2px solid green'}} onClick={addCrypto}>Increase Stocks in crypto</button>
+      <button style={{color: 'white', backgroundColor: 'red' }} onClick={deductCrypto}>manage stocks</button>
+      <button onClick={jumpadd5}>Add 5 more</button>
+      <button onClick={jumpdeduct5}>deduct 5 </button>
+    </div>
+  );
+
+}
+
+export default CryptoContainer;
